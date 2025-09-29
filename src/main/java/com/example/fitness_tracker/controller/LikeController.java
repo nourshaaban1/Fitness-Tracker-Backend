1f2f08ea0567e84169b3bcd416a1d8c8d164c057
RamezRagaay <ramez5ragay@gmail.com>
2025-09-29 05:39:57 +0300

    Feat: Added features to the community: (boolean) hasUserLike

--- src/main/java/com/example/fitness_tracker/controller/LikeController.java
+++ src/main/java/com/example/fitness_tracker/controller/LikeController.java
@@ -1,7 +1,5 @@
 package com.example.fitness_tracker.controller;
 
-import com.example.fitness_tracker.domain.dto.Community.PostLikesResponse;
-import com.example.fitness_tracker.domain.models.Like;
 import com.example.fitness_tracker.security.JwtService;
 import com.example.fitness_tracker.service.AuthService;
 import com.example.fitness_tracker.service.LikeService;
@@ -10,8 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -25,9 +21,6 @@ public class LikeController {
 
     private final AuthService authService;
 
-
-
-
     @PostMapping
     public ResponseEntity<Void> likePost(@PathVariable UUID postId, @RequestHeader("Authorization") String authHeader)
     {
@@ -38,21 +31,20 @@ public class LikeController {
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping()
+    @GetMapping("/hasUserLiked")
+    public ResponseEntity<Boolean> hasUserLiked(@PathVariable UUID postId, @RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.replace("Bearer ", "");
+        UUID userId = jwtService.extractUserId(token);
+        return ResponseEntity.ok(likeService.hasUserLiked(postId, userId));
+    }
+
+    @DeleteMapping
     public ResponseEntity<Void> unlikePost(@PathVariable UUID postId, @RequestHeader("Authorization") String authHeader) {
         String token = authHeader.replace("Bearer ", "");
         UUID userId = jwtService.extractUserId(token);
         likeService.unlikePost(postId, userId);
         return ResponseEntity.noContent().build();
     }
-    
-    /**
-     * Get the number of likes and list of user IDs who liked a specific post
-     * @param postId The ID of the post
-     * @return PostLikesResponse containing total likes and list of user IDs
-     */
-    @GetMapping()
-    public ResponseEntity<PostLikesResponse> getPostLikes(@PathVariable UUID postId) {
-        return ResponseEntity.ok(likeService.getPostLikesResponse(postId));
-    }
+
+
 }
---
+++ src/main/java/com/example/fitness_tracker/domain/models/PostLike.java
@@ -0,0 +1,26 @@
+package com.example.fitness_tracker.domain.models;
+
+
+import com.example.fitness_tracker.domain.models.auditable.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "PostLike")
+@Getter
+@Setter
+public class PostLike extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
+
